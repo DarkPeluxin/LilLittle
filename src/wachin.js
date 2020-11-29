f860e135f2c73bf0eb2d4ee34751056b2914c3cd
BotPelu <zeroutwo1@gmail.com>
2020-11-28 22:12:51 -0500

    server fue modificado

--- src/wachin.js
+++ src/wachin.js
@@ -10,7 +10,7 @@ client.on('ready', () => {
 
     client.user.setPresence( {
         activity: {
-            name: `v. 1.0.7`,
+            name: `v. 1.3.1b`,
             type: "PLAYING"
         },
         status: "idle"
@@ -18,77 +18,126 @@ client.on('ready', () => {
 });
 
 client.on('message', async message => {
-
     const args = message.content.slice(prefix.length).trim().split(/ +/g)
     const command = args.shift().toLowerCase();
-    const user = message.mentions.users.first();
 
-    if(command === 'clean'){
+    if(command === 'redes') {
+        message.channel.send({embed: {
+            color: 3447003,
+            title: "Sígueme y entérate de todo.",
+            description: "_Aquí puedes encontrarme_.",
+            fields: [{
+                name: "FACEBOOK",
+                value: "https://fg.gg.DarkPeluxin"
+            },
+            {   
+                name: "YOUTUBE",
+                value: "https://youtube.com/DarkPeluxin"
+            }
+         ],
+         timestamp: new Date (),
+         footer: {
+             icon_url: client.user.avatarURL(),
+             text: "Trabajando duro como tu viejita."
+         }
+        }
+        })
+        message.delete();
+    };
+   
+    
+    if(command === 'ñofi') {
+        let user = message.mentions.users.first();
+        if (!user) return message.channel.send(`¿Se supone que tengo que adivinar a quién botar? Idiota.`)
+        let razon = args.slice(1).join(' ');
 
         var perms = message.member.hasPermission("KICK_MEMBERS");
-        if(!perms) return message.channel.send("Tas bajito loco.");
+        if (!perms) return message.channel.send("Tas bajito loco.");
+        if (!razon) return message.channel.send('Pero el ¿porqué? pé tarao.');
+        if (!message.guild.member(user).kickable) return message.reply('¿Qué intentas hacer Mongol?!');
+
+        message.guild.member(user).kick(razon);
+        message.channel.send(`:mouse:     ${user.toString()} está ñofi de **Los Wachines**`)
+        const embed = new Discord.MessageEmbed()
+        .setDescription(`Razón: _${razon}_`)
+        .setColor("RED")
+        message.channel.send(embed);
+        message.delete();
+    };
+
+    if(command == 'clean') {
+
+        var perms = message.member.hasPermission("MANAGE_CHANNELS");
+        if(!perms) return message.channel.send("Bajito, no tienes poderes.");
         let cantidad = parseInt(args[0]);
         message.channel.bulkDelete(cantidad);
     };
 
-    // Receiving the message
-    console.log(message.content);
-    if (message.content === 'chupa'){
-        message.channel.send('gampi');
-    }
+    if(command === 'shh') {
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('Bajito, no tienes poder.')
 
-    // Receiving the message
-    console.log(message.content);
+    let channels = message.guild.channels.cache.filter(ch => ch.type !== 'category');
+    let user = message.mentions.members.first()
+    let razon = args.slice(1).join(" ");
+    if(!razon) return message.channel.send('Pero el ¿por qué? pé tarao.');
+
+    channels.forEach(channel => {
+        if(channel.name == message.channel.name){
+            channel.updateOverwrite( user , {
+                SEND_MESSAGES: false,
+                ADD_REACTIONS: false
+            })
+        }
+    })
+
+    message.channel.send(`:microphone2:   ${message.author} le cerró el poto a ${user.toString()}`)
+       const embed = new Discord.MessageEmbed()
+       .setDescription(`Razón: _${razon}_`)
+       .setColor("BLUE")
+       message.channel.send(embed);
+       message.delete();
+    };
+    
+    if(command === "call") {
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('Bajito, no tienes poder.')
+
+        let channels = message.guild.channels.cache.filter(ch => ch.type !== 'category');
+        let user = message.mentions.members.first()
+    
+        channels.forEach(channel => {
+            if(channel.name == message.channel.name){
+                channel.updateOverwrite( user , {
+                    SEND_MESSAGES: true,
+                    ADD_REACTIONS: true
+                })
+            }
+        })
+
+        message.channel.send(`:microphone2:  ${message.author} le abrió el ano a ${user.toString()}, ahora puedes hablar.`)
+        message.delete();
+
+    };
+
     if (message.content === 'Chupa'){
         message.channel.send('gampi');
     }
 
-    if(command === 'ñofi'){
-    let user = message.mentions.users.first();
-    if (!user) return message.channel.send(`¿Se supone que tengo que adivinar a quién botar? Idiota`)
-
-    let razon = args.slice(1).join(' ');
-
-    var perms = message.member.hasPermission("KICK_MEMBERS");
-    if (!perms) return message.channel.send("Tas bajito loco.");
-    if (!razon) return message.channel.send('Pero el ¿porqué? pé tarao.');
-    if (!message.guild.member(user).kickable) return message.reply('¿Qué intentas hacer Mongol?!');
-
-    message.guild.member(user).kick(razon);
-    message.channel.send(`:mouse:     ${user.toString()} está ñofi de **Los Wachines**`)
-    const embed = new Discord.MessageEmbed()
-     .setDescription(`Razón: _${razon}_`)
-     .setColor("RED")
-     message.channel.send(embed);
+    if (message.content === 'chupa'){
+        message.channel.send('gampi');
     }
 
-    if(!message.content.startsWith(prefix)) return;
-    if(message.author.bot) return;
+    if (message.content === 'CHUPA'){
+        message.channel.send('gampi');
+    }
 
-    if(command === 'redes')
+    if (message.content === 'Chvp4'){
+        message.channel.send('gampi');
+    }
 
-       message.channel.send({embed: {
-           color: 3447003,
-           title: "Sígueme y entérate de todo.",
-           description: "_Aquí puedes encontrarme_.",
-           fields: [{
-               name: "FACEBOOK",
-               value: "https://fg.gg.DarkPeluxin"
-           },
-           {   
-               name: "YOUTUBE",
-               value: "https://youtube.com/DarkPeluxin"
-           }
-        ],
-        timestamp: new Date (),
-        footer: {
-            icon_url: client.user.avatarURL(),
-            text: "Trabajando duro como tu viejita."
-        }
-       }
-    });
+    if (message.content === 'chvp4'){
+        message.channel.send('gampi');
+    }
 
-    
 });
 
 client.on("messageDelete", (message) => {
